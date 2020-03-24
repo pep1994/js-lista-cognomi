8730cce4757345541bf454ac08083a9704974f8c
Marco Petrini <marcopetrini1994@gmail.com>
2020-03-24 14:36:45 +0100

    logica di stampa lista

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // Scrivi anche la posizione della lista in cui il nuovo utente si trova
 
 
-var userSurname, listSurnameOrder;
+var userSurname, listSurnameOrder, itemListSurnameOrder, backContent;
 
 // creo array con la lista dei cognomi
 var listSurname = ["Verdi", "Rossi", "Bianchi", "Neri", "Stark", "Lannister", "Martell", "Baratheon"];
@@ -22,3 +22,15 @@ listSurnameOrder = listSurname.sort();
 
 
 // stampo la lista
+var i = 0;
+
+while (i < listSurnameOrder.length) {
+  itemListSurnameOrder = listSurnameOrder[i];
+
+  backContent = document.getElementById('surname-list').innerHTML;
+
+  document.getElementById('surname-list').innerHTML = backContent + itemListSurnameOrder;
+  console.log(backContent + itemListSurnameOrder);
+
+  i++;
+}
